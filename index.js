12697c0a89409b3e3a4964754debce0d74dd6f5a
Josiah-Canada <josiahcanada@yahoo.com>
2024-08-16 13:59:23 -0400

    cleanup

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+var images = new Array('images/gyal.jpg','images/butterfly.jpg','images/rings.jpg','images/brooch.jpg')
+var nextImage = 0
+
+slideShow()
+
+function slideShow() {
+if(nextImage>=images.length) {nextImage=0}
+$('.content')
+.css('background-image', 'url("'+ images[nextImage++]+'")')
+.fadeIn(500, function(){
+    setTimeout(slideShow, 1000)
+})
+}
+
+
+
 
 
 
